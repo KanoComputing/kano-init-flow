1d3eaa262229123d345300253003139d515d7a1b
asimon <alejandro@kano.me>
2014-07-29 11:04:58 +0100

    Use generic function to change wallpaper

--- kano_init_flow/display_screen.py
+++ kano_init_flow/display_screen.py
@@ -15,51 +15,12 @@ from kano_init_flow.reboot_screen import RebootScreen
 from kano_init_flow.data import get_data
 import kano_init_flow.constants as constants
 from kano_settings.set_display.functions import get_overscan_status, write_overscan_values, set_overscan_status
+from kano_settings.set_wallpaper import change_wallpaper
 import kano.gtk3.kano_dialog as kano_dialog
 
 kdeskrc_home = "/home/%s/.kdeskrc"
 wallpaper_path = "/usr/share/kano-desktop/wallpapers/"
 overscan_pipe = "/var/tmp/overscan"
-
-
-def change_wallpaper(path, name):
-    # home directory
-    USER = os.environ['SUDO_USER']
-    deskrc_path = kdeskrc_home % (USER)
-    # Wallpaper selected
-    image_169 = "%s%s-16-9.png" % (path, name)
-    image_43 = "%s%s-4-3.png" % (path, name)
-    image_1024 = "%s%s-1024.png" % (path, name)
-    # Look for the strings
-    found = False
-    if os.path.isfile(deskrc_path):
-        f = open(deskrc_path, 'r')
-        newlines = []
-        for line in f:
-            if "Background.File-medium:" in line:
-                line = "  Background.File-medium: %s\n" % (image_1024)
-                found = True
-            elif "Background.File-4-3:" in line:
-                line = "  Background.File-4-3: %s\n" % (image_43)
-            elif "Background.File-16-9:" in line:
-                line = "  Background.File-16-9: %s\n" % (image_169)
-            newlines.append(line)
-        f.close()
-    if found:
-        # Overwrite config file with new lines
-        outfile = open(deskrc_path, 'w')
-        outfile.writelines(newlines)
-        outfile.close()
-    # If not found add it
-    else:
-        with open(deskrc_path, "a") as outfile:
-            outfile.write("  Background.File-medium: %s\n" % (image_1024))
-            outfile.write("  Background.File-4-3: %s\n" % (image_43))
-            outfile.write("  Background.File-16-9: %s\n" % (image_169))
-    # Refresh the wallpaper
-    cmd = 'sudo -u %s kdesk -w' % USER
-    os.system(cmd)
-    return 0
 
 
 class DisplayScreen():
