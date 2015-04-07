4defb5eb7329ff50e54559a33d0cddd59059667e
Alex Burr <alex.burr@kano.me>
2015-04-07 13:24:57 +0100

    Add facility in background to call a callback on click/keypress.

--- kano_init_flow/faux_panel.py
+++ kano_init_flow/faux_panel.py
@@ -7,17 +7,25 @@
 
 from gi.repository import Gtk
 
+# For icon state
+GRAY = 0
+ON = 1
+OFF = 2
+
+# for setting icon callback
+WIFI = 1
+SETTINGS = 1
+
 
 class FauxPanel(Gtk.EventBox):
-        # hthing
-        # K menu image
-        # expander
-        # settings image
-        # wifi image
-        # speaker image
-        # clock (label?)
-    def __init__(self):
-        Gtk.EventBox.__init__(self)        
+
+    # parent.set_bg_callback is used to collect events form the background in case
+    # the user misses. Today we only do the same thing that an icon click does,
+    # for the future we may flash the icon to indicate that the user should click on it.
+    def __init__(self, parent):
+        Gtk.EventBox.__init__(self)
+
+        self._parent = parent
 
         self._box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         self.add(self._box)
