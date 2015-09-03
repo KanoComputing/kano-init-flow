cd01448cd1c4517f75e28103c0657255660866dd
Radek Pazdera <radek@kano.me>
2015-09-03 17:16:43 +0100

    Adding TODO

--- kano_init_flow/stages/wifi/main.py
+++ kano_init_flow/stages/wifi/main.py
@@ -41,6 +41,7 @@ class Wifi(Stage):
         self._ctl.main_window.push(s1.widget)
 
     def second_scene(self):
+        # TODO: uncomment before release
         # Jump directly at the end if we have internet
         #if is_internet():
         #    self.third_scene()
